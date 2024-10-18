c7fabf5e2c03fe448b4d8585227784a641a93b5b
tomvdt <tomtommy135@gmail.com>
2024-10-18 11:44:48 +0200

    all the analysis are belong to us

--- 02-plasma-1/analyse/constants.py
+++ 02-plasma-1/analyse/constants.py
@@ -5,12 +5,8 @@ from numpy import pi, mean
 CM_PER_INCH = 2.54  # cm/inch
 INCH_PER_CM = 1/2.54 # inch/cm
 
-GAMMA_CS137 = 662  # keV
-GAMMA_PB210 = 46.5  # keV
-GAMMA_CO57 = 121.9  # keV
-GAMMA_HF181 = 482 - 136  # keV
+ELEMENTARY_CHARGE = 1.602_176_634e-19  # C
+BOLTZMANN_CONSTANT = 1.380_649e-23  # J/K
+ELECTRON_MASS = ufloat(9.109_383_7139e-31, 0.000_000_0028e-31)  # kg
 
-# Uncertainties
-ERROR_ATTENUATION_COUNT = 0.01  # %
-ERROR_TIME = 0.1  # s
-ERROR_THICC = 1e-3  # mm
+SURFACE_PROBE = 1.26e-5  # m^2
