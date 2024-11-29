91d02b9f4ea6504c535548013c4a47c2087403fe
tomvdt <tomtommy135@gmail.com>
2024-11-29 14:24:44 +0100

    2d gaussian!

--- 03-microscopie-fluorescence/analyse/constants.py
+++ 03-microscopie-fluorescence/analyse/constants.py
@@ -4,13 +4,3 @@ from numpy import pi, mean
 # Real constants
 CM_PER_INCH = 2.54  # cm/inch
 INCH_PER_CM = 1/2.54 # inch/cm
-
-GAMMA_CS137 = 662  # keV
-GAMMA_PB210 = 46.5  # keV
-GAMMA_CO57 = 121.9  # keV
-GAMMA_HF181 = 482 - 136  # keV
-
-# Uncertainties
-ERROR_ATTENUATION_COUNT = 0.01  # %
-ERROR_TIME = 0.1  # s
-ERROR_THICC = 1e-3  # mm
