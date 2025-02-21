b0a461c0d51c6b7022057cfd37e6cc6093d21c71
tomvdt <tomtommy135@gmail.com>
2025-02-21 10:28:02 +0100

    tpIII is back babyyyyyyyy

---
+++ 04-radiotelescope/analyse/constants.py
@@ -0,0 +1,10 @@
+from uncertainties import ufloat
+from numpy import pi, mean
+
+# Real constants
+CM_PER_INCH = 2.54  # cm/inch
+INCH_PER_CM = 1/2.54 # inch/cm
+
+# Camera
+NM_PER_PX = 108  # nm/px
+NUMERICAL_APERTURE = 1.25
