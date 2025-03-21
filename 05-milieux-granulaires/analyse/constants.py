0e1b29cc2e68bc76505a3105f8deaab0b632b283
tomvdt <tomtommy135@gmail.com>
2025-03-21 11:37:24 +0100

    first dataset + analysus

--- 05-milieux-granulaires/analyse/constants.py
+++ 05-milieux-granulaires/analyse/constants.py
@@ -1,6 +1,15 @@
 from uncertainties import ufloat
-from numpy import pi
+import numpy as np
+from matplotlib import colors
 
 # Real constants
 CM_PER_INCH = 2.54  # cm/inch
 INCH_PER_CM = 1/2.54 # inch/cm
+
+
+# https://stackoverflow.com/questions/18926031/how-to-extract-a-subset-of-a-colormap-as-a-new-colormap-in-matplotlib
+def truncate_colormap(cmap, minval=0.0, maxval=1.0, n=100):
+    new_cmap = colors.LinearSegmentedColormap.from_list(
+        'trunc({n},{a:.2f},{b:.2f})'.format(n=cmap.name, a=minval, b=maxval),
+        cmap(np.linspace(minval, maxval, n)))
+    return new_cmap
