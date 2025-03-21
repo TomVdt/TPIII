e4f886c0adeb7e3d8c60954719743989b3926adb
mVenez <matteo.veneziano@ikmail.com>
2025-03-21 13:56:37 +0100

    Teff, doesn't work

--- 05-milieux-granulaires/analyse/constants.py
+++ 05-milieux-granulaires/analyse/constants.py
@@ -6,6 +6,8 @@ from matplotlib import colors
 CM_PER_INCH = 2.54  # cm/inch
 INCH_PER_CM = 1/2.54 # inch/cm
 
+BOLTZMANN_CONSTANT_JOULE = 1.380_649e-23  # J/K
+
 
 # https://stackoverflow.com/questions/18926031/how-to-extract-a-subset-of-a-colormap-as-a-new-colormap-in-matplotlib
 def truncate_colormap(cmap, minval=0.0, maxval=1.0, n=100):
