fcf9f72b165e8608afa990e8a02749d67cfd95a5
tomvdt <tomtommy135@gmail.com>
2025-03-21 15:34:32 +0100

    refactor

--- 05-milieux-granulaires/analyse/constants.py
+++ 05-milieux-granulaires/analyse/constants.py
@@ -1,17 +1,7 @@
 from uncertainties import ufloat
-import numpy as np
-from matplotlib import colors
 
 # Real constants
 CM_PER_INCH = 2.54  # cm/inch
 INCH_PER_CM = 1/2.54 # inch/cm
 
 BOLTZMANN_CONSTANT_JOULE = 1.380_649e-23  # J/K
-
-
-# https://stackoverflow.com/questions/18926031/how-to-extract-a-subset-of-a-colormap-as-a-new-colormap-in-matplotlib
-def truncate_colormap(cmap, minval=0.0, maxval=1.0, n=100):
-    new_cmap = colors.LinearSegmentedColormap.from_list(
-        'trunc({n},{a:.2f},{b:.2f})'.format(n=cmap.name, a=minval, b=maxval),
-        cmap(np.linspace(minval, maxval, n)))
-    return new_cmap
---
+++ 05-milieux-granulaires/analyse/lib.py
@@ -0,0 +1,63 @@
+import numpy as np
+from matplotlib import colors
+from dataclasses import dataclass
+from glob import glob
+
+@dataclass
+class Step:
+    filename: str
+    imposed_vibration: float
+    gamma: float
+    step_time: float
+    freqs: np.ndarray
+    psd: np.ndarray
+    chi_abs: np.ndarray
+    chi_im: np.ndarray
+
+# Load data
+def load(dataset_name: str) -> list[Step]:
+    metadata_file = glob(f'../data/{dataset_name}/*-exp?.txt')
+    metadata = np.loadtxt(metadata_file[0], skiprows=1, delimiter='\t')
+
+    data_files = glob(f'../data/{dataset_name}/*-exp?_step*.txt')
+    data_files.sort(key=lambda s: int(s.split('step')[-1][:-4]))
+    data: list[Step] = []
+
+    for i, file in enumerate(data_files):
+        try:
+            freq, psd, chi_abs, chi_im = np.loadtxt(file, skiprows=1, delimiter='\t', unpack=True)
+        except ValueError:
+            print(f"WARNING: data is fucked for {file}")
+            continue
+
+        imposed_vibration, _, gamma, step_time, *_ = metadata[i]
+
+        if np.allclose(chi_abs, 0): # Data is corrupted when freq_response contains only zeros
+            print(f"WARNING: data is fucked for {file}")
+            continue
+        if np.allclose(freq, 0):
+            print(f"WARNING: data is fucked for {file}, trying to recover freqs...")
+            freq = np.arange(5, 70 + 0.0001, 0.1625)
+
+        data.append(
+            Step(
+                file,
+                imposed_vibration, gamma, step_time,
+                freq, psd, chi_abs, chi_im
+            )
+        )
+    
+    return data
+
+# Functions for fits
+def modulus_chi(x: np.ndarray, I: float, w0: float, alpha: float) -> np.ndarray:
+    return 1 / np.sqrt(
+        (I * (w0**2 - x**2))**2 + (alpha * x)**2
+    )
+
+# https://stackoverflow.com/questions/18926031/how-to-extract-a-subset-of-a-colormap-as-a-new-colormap-in-matplotlib
+def truncate_colormap(cmap, minval=0.0, maxval=1.0, n=100):
+    new_cmap = colors.LinearSegmentedColormap.from_list(
+        'trunc({n},{a:.2f},{b:.2f})'.format(n=cmap.name, a=minval, b=maxval),
+        cmap(np.linspace(minval, maxval, n)))
+    return new_cmap
