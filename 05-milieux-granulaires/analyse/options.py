b452d1982a3cbcb3b6abc5d9ffa1d6252d9e4499
mVenez <matteo.veneziano@ikmail.com>
2025-04-08 18:41:43 +0200

    Plots are now C H O N K E R

--- 05-milieux-granulaires/analyse/options.py
+++ 05-milieux-granulaires/analyse/options.py
@@ -1,3 +1,5 @@
+from constants import *
+
 rcParams = {
     # Text appearance
     "text.usetex": True,
@@ -11,39 +13,55 @@ rcParams = {
     ]),
 
     # Text size
-    "axes.labelsize": 12,
-    "font.size": 12,
-    "legend.fontsize": 12,
-    "xtick.labelsize": 12,
-    "ytick.labelsize": 12,
-    # 'lines.markersize': 12,
+    "axes.labelsize": 16,
+    "font.size": 16,
+    "legend.fontsize": 16,
+    "xtick.labelsize": 16,
+    "ytick.labelsize": 16,
+    # 'lines.markersize': 16,
 
     # Figure options
-    "figure.figsize": (3.95, 2.96),
-    'figure.dpi': 130,
-    'axes.titlesize': 12,
+    "figure.figsize": (12*INCH_PER_CM, 12*INCH_PER_CM),
+    'figure.dpi': 150,
+    'axes.titlesize': 16,
+    'axes.linewidth': 1.5,
     'lines.color': 'grey',
-    'lines.linewidth' : 1,
+    'lines.linewidth' : 1.5,
     'scatter.marker': '+',
-    'errorbar.capsize': 4,
+    'errorbar.capsize': 3,
     'axes.formatter.useoffset': False,
     # 'axes.spines.right': False,  # spineless smh
     # 'axes.spines.top': False,
 
+    # Ticks
+    'xtick.major.size' : 6,
+    'xtick.major.width' : 1.5,
+    'xtick.minor.size' : 3,
+    'xtick.minor.width' : 1,
+
+    'ytick.major.size' : 6,
+    'ytick.major.width' : 1.5,
+    'ytick.minor.size' : 3,
+    'ytick.minor.width' : 1,
+
+    'xtick.minor.visible' : True,
+    'ytick.minor.visible' : True,
+    
     # Legend
     'legend.framealpha': 1.0,
     "legend.handlelength" : 1.2,
     "legend.handleheight" : 0.5,
     "legend.handletextpad" : 0.6,
     "legend.labelspacing" : 0.3,
+    'legend.edgecolor':   'black' ,
+    'patch.linewidth' : 1.5,
 
+    # Grid
     'axes.axisbelow' : True,
     'axes.grid' : True,
     'grid.linestyle' : '-',
-    'grid.linewidth': 0.5,
+    'grid.linewidth': 1,
     'grid.alpha': 0.75,
-    'xtick.minor.visible' : True,
-    'ytick.minor.visible' : True,
 
     # Saving
     # 'savefig.bbox': 'tight', # ça donne des figures avec marges irreguliers. 
--- 05-milieux-granulaires/analyse/plot_alpha.py
+++ 05-milieux-granulaires/analyse/plot_alpha.py
@@ -16,18 +16,45 @@ std_devs = unp.std_devs
 
 mpl.rcParams.update(rcParams)
 
+# mpl.rcParams['savefig.bbox'] = 'standard'
+mpl.rcParams["figure.figsize"] = (12*INCH_PER_CM, 12*INCH_PER_CM)
+# mpl.rcParams["figure.subplot.left"]   = 0.21
+# mpl.rcParams['figure.subplot.right']  = 0.75
+# mpl.rcParams['figure.subplot.top']    = 0.97
+# mpl.rcParams['figure.subplot.bottom'] = 0.2
+
 # params
 dir = "../data/"
+c_glass = 'C0'
+c_plastic = 'C3'
+c_sand = 'C1'
 
-# Data loading
+# Data loading      # TODO: DESSINER FITS
 glass_im = np.load(f"{dir}glass_alpha_im.npz", allow_pickle=True)
 glass_im_amplitude, glass_im_alpha = glass_im["arr_0"], glass_im["arr_1"]
+plastic_im = np.load(f"{dir}plastic_alpha_im.npz", allow_pickle=True)
+plastic_im_amplitude, plastic_im_alpha = plastic_im["arr_0"], plastic_im["arr_1"]
+sand_im = np.load(f"{dir}sand_alpha_im.npz", allow_pickle=True)
+sand_im_amplitude, sand_im_alpha = sand_im["arr_0"], sand_im["arr_1"]
 
 
 # Plot
+fig, ax = plt.subplots(1,1)
+
 plt.errorbar(glass_im_amplitude, nom_vals(glass_im_alpha),
              yerr=std_devs(glass_im_alpha),
-             ls=''
+             ls='', c=c_glass,
+             label='Glass'
+             )
+plt.errorbar(plastic_im_amplitude, nom_vals(plastic_im_alpha),
+             yerr=std_devs(plastic_im_alpha),
+             ls='', c=c_plastic,
+             label='Plastic'
+             )
+plt.errorbar(sand_im_amplitude, nom_vals(sand_im_alpha),
+             yerr=std_devs(sand_im_alpha),
+             ls='', c=c_sand,
+             label='Sand'
              )
 
 plt.yscale('log')
@@ -35,7 +62,8 @@ plt.xscale('log')
 plt.xlabel(r'Oscillation amplitude [V]')
 plt.ylabel(r'Friction coefficient $\alpha$')
 
-plt.tight_layout()
-plt.savefig('../figures/alpha_together.png')
+ax.legend(bbox_to_anchor=(1.02,1))
 
-plt.show()
+plt.savefig('../figures/alpha_together.png', bbox_inches='tight')
+
+# plt.show()
