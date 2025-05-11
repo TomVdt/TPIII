bf7099d68e9cb0e8f744d8a208eaafd38c0195ab
tomvdt <tomtommy135@gmail.com>
2025-05-11 17:03:17 +0200

    some progress on schottky barrier

--- 06-photoemission-interne/analyse/constants.py
+++ 06-photoemission-interne/analyse/constants.py
@@ -4,9 +4,15 @@ from numpy import pi, log10
 # Real constants
 CM_PER_INCH = 2.54  # cm/inch
 INCH_PER_CM = 1/2.54 # inch/cm
+EV_PER_JOULE = 6.241509e+18  # eV / J
 
 # Calibration
 RESISTANCE_73K = 18.49
 RESISTANCE_303K = 111.67
 RESISTANCE_SLOPE = (303 - 73) / (RESISTANCE_303K - RESISTANCE_73K)  # dT/dR
 RESISTANCE_OFFSET = 303 - RESISTANCE_SLOPE * RESISTANCE_303K
+
+# Actual constant
+
+A_G = 1.12e6  # A / m² / K² [notice]
+A = 1e-4  # m² [aire de la diode, estimated]
--- 06-photoemission-interne/analyse/lib.py
+++ 06-photoemission-interne/analyse/lib.py
@@ -18,22 +18,26 @@ def load(path: str) -> list[Data]:
     data_files.sort()
     data = []
     for i, file in enumerate(data_files):
+        # print(file, i)
         # V [V], I [mA]
         V, I = np.loadtxt(file, delimiter='\t', unpack=True,converters=lambda s: s.replace(',', '.'))
+        # Extract temperature (Ohm)
+        ohm_lower, ohm_upper, *_ = map(float, file.split("/")[-1][:-4].split('-'))
+        T = (resistance_to_temperature(ohm_lower) + resistance_to_temperature(ohm_upper)) / 2
         data.append(
             Data(
                 filename=file,
                 I=I[V>0],
                 V=V[V>0],
-                temperature=300
+                temperature=T
             )
         )
     return data
 
 def plot_IV(data: Data):
-    plt.plot(data.V, data.I)
-    plt.xlabel('Tension [V]')
-    plt.ylabel('Current [mA]')
+    plt.plot(data.I, data.V)
+    plt.xlabel('Current [mA]')
+    plt.ylabel('Voltage [V]')
 
 def resistance_to_temperature(ohm: float) -> float:
     return ohm * RESISTANCE_SLOPE + RESISTANCE_OFFSET
@@ -42,4 +46,4 @@ def curve_IV(I: np.ndarray[float], T, I_s, n, R) -> np.ndarray[float]:
     return (n*k*T / elementary_charge) * np.log(I/I_s + 1) + R*I
 
 if __name__ == '__main__':
-    print(resistance_to_temperature(80))
+    print(resistance_to_temperature(109) - 273.15)
