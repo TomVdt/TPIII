15c709256aa5d3f57662da71bd5a2927d994d2e3
mVenez <matteo.veneziano@ikmail.com>
2025-05-21 09:52:28 +0200

    Easier data loading

--- 06-photoemission-interne/analyse/lib.py
+++ 06-photoemission-interne/analyse/lib.py
@@ -7,7 +7,7 @@ from glob import glob
 
 
 @dataclass
-class Data:
+class IV:
     filename: str
     I: np.ndarray[float]
     V: np.ndarray[float]
@@ -22,7 +22,15 @@ class Calibration:
     lambd: np.ndarray[float]
     intensity: np.ndarray[float]
 
-def load(path: str) -> list[Data]:
+@dataclass
+class Photocurrent:
+    min_lambda: float
+    max_lambda: float
+    lambd: np.ndarray[float]
+    intensity: np.ndarray[float]
+    E: np.ndarray[float]
+
+def load_IV(path: str) -> list[IV]:
     data_files = glob(f'../data/{path}/*.tsv')
     data_files.sort()
     data = []
@@ -35,7 +43,7 @@ def load(path: str) -> list[Data]:
         T = (resistance_to_temperature(ohm_lower) + resistance_to_temperature(ohm_upper)) / 2
         err_T = abs(resistance_to_temperature(ohm_lower) - resistance_to_temperature(ohm_upper)) / 2
         data.append(
-            Data(
+            IV(
                 filename=file,
                 I=I[V>0],
                 V=V[V>0],
@@ -44,6 +52,40 @@ def load(path: str) -> list[Data]:
         )
     return data
 
+def load_calibration(path: str) -> list[Calibration]:
+    data_files = glob(f'../data/{path}/*.tsv')
+    data_files.sort()
+    dataset: list[Calibration] = []
+    for i, file in enumerate(data_files):
+        _, t, signal, _ = np.loadtxt(file, unpack=True, delimiter="\t", converters=lambda s: s.replace(',', '.'))
+        sensor, filterr, lower, upper = file.split('/')[-1][:-4].split('-')
+        lower = int(lower)
+        upper = int(upper)
+        filterr = int(filterr)
+
+        data = Calibration(
+            sensor=sensor, filter=filterr, min_lambda=lower, max_lambda=upper,
+            lambd=np.linspace(lower, upper, len(t), endpoint=True),
+            intensity=signal
+        )
+
+        dataset.append(data)
+    return dataset
+
+def load_photocurrent(path:str) -> list[Photocurrent]:
+    data_files = glob(f'../data/{path}/*.tsv')
+    data_files.sort()
+    dataset: list[Calibration] = []
+    for i, file in enumerate(data_files):
+        _, t, signal, _ = np.loadtxt(file, unpack=True, delimiter="\t", converters=lambda s: s.replace(',', '.'))
+        test_lower = 200
+        test_upper = 2600
+        lambd = np.linspace(test_lower, test_upper, len(t), endpoint=True) # [nm]
+        E = h * c / (lambd*1e-9) * EV_PER_JOULE # [eV]
+
+        dataset.append(Photocurrent(test_lower, test_upper, lambd, signal, E))
+    return dataset
+    
 def resistance_to_temperature(ohm: float) -> float:
     return ohm * RESISTANCE_SLOPE + RESISTANCE_OFFSET
 
